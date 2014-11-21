6195aafef411d59ad62ac8d66cc1d8d83477fc56
T.J. Purtell <tj@mobisocial.us>
2014-11-20 19:20:41 -0800

    curve25519

---
+++ csharp/crypto/runsdvui.cmd
@@ -0,0 +1,2 @@
+cd /d "C:\Users\tj\Developer\bccrypto\csharp\crypto" &msbuild "cryptomath.csproj" /t:sdvViewer /p:configuration="Debug" /p:platform=Any CPU
+exit %errorlevel% 
--- csharp/crypto/src/util/BigIntegers.cs
+++ csharp/crypto/src/util/BigIntegers.cs
@@ -4,29 +4,46 @@ using Org.BouncyCastle.Math;
 
 namespace Org.BouncyCastle.Utilities
 {
-	/**
-	 * BigInteger utilities.
-	 */
-	public sealed class BigIntegers
-	{
-		private const int MaxIterations = 1000;
+    /**
+     * BigInteger utilities.
+     */
+    public abstract class BigIntegers
+    {
+        private const int MaxIterations = 1000;
 
-		private BigIntegers()
-		{
-		}
+        /**
+        * Return the passed in value as an unsigned byte array.
+        *
+        * @param value value to be converted.
+        * @return a byte array without a leading zero byte if present in the signed encoding.
+        */
+        public static byte[] AsUnsignedByteArray(
+            BigInteger n)
+        {
+            return n.ToByteArrayUnsigned();
+        }
 
-		/**
-		* Return the passed in value as an unsigned byte array.
-		*
-		* @param value value to be converted.
-		* @return a byte array without a leading zero byte if present in the signed encoding.
-		*/
-		public static byte[] AsUnsignedByteArray(
-			BigInteger n)
-		{
-			return n.ToByteArrayUnsigned();
-		}
+        /**
+         * Return the passed in value as an unsigned byte array of specified length, zero-extended as necessary.
+         *
+         * @param length desired length of result array.
+         * @param n value to be converted.
+         * @return a byte array of specified length, with leading zeroes as necessary given the size of n.
+         */
+        public static byte[] AsUnsignedByteArray(int length, BigInteger n)
+        {
+            byte[] bytes = n.ToByteArrayUnsigned();
 
+            if (bytes.Length > length)
+                throw new ArgumentException("standard length exceeded", "n");
 
-	}
+            if (bytes.Length == length)
+                return bytes;
+
+            byte[] tmp = new byte[length];
+            Array.Copy(bytes, 0, tmp, tmp.Length - bytes.Length, bytes.Length);
+            return tmp;
+        }
+
+    }
 }
